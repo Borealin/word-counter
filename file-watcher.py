f1a4a5d5c97785974ab712d5780299baffb74413
Borealin <308704199deniel@gmail.com>
2022-05-16 11:31:10 +0800

    [ADD] add remain time and type hint

--- file-watcher.py
+++ file-watcher.py
@@ -1,26 +1,66 @@
+import argparse
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 from functools import reduce
 import json
 from pathlib import Path
 import tkinter as tk
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileSystemEvent, EVENT_TYPE_MODIFIED
 
 from queue import Queue
 import sys
 import subprocess
-import time
+from fastclasses_json import dataclass_json, JSONMixin
 
 
 def texcount(file) -> int:
     res = subprocess.check_output(['texcount', '-brief', file]).decode('utf-8')
     return int(res.split('+')[0])
 
+
+@dataclass_json
 @dataclass
-class Config:
+class RawWatchFile:
     filename: str
     display: str
+
+    def to_watch_file(self) -> "WatchFile":
+        return WatchFile(Path(self.filename), self.display)
+
+
+@dataclass
+class WatchFile:
+    path: Path
+    display: str
+
+
+@dataclass_json
+@dataclass
+class RawConfigs(JSONMixin):
+    files: List["RawWatchFile"]
+    ddl: str
+    show_total: bool = False
+    time_format = '%Y-%m-%d %H:%M'
+
+    def to_configs(self) -> "Configs":
+        return Configs(
+            [f.to_watch_file() for f in self.files],
+            datetime.strptime(self.ddl, self.time_format),
+            self.show_total
+        )
+
+    @classmethod
+    def from_json(cls, json_data: Union[str, bytes], *, infer_missing=True) -> "RawConfigs":
+        pass
+
+
+@dataclass
+class Configs:
+    files: List["WatchFile"]
+    ddl: datetime
+    show_total: bool = False
 
 
 def create_label(
@@ -28,7 +68,7 @@ def create_label(
     display: str,
     count: int,
     stringvar: tk.StringVar,
-    font=('Cascadia Code', '16'),
+    font,
 ):
     stringvar.set(str(count))
     frame = tk.Frame(master)
@@ -43,66 +83,92 @@ class App(object):
     WATCHDOG_EVENT = "<<WatchdogEvent>>"
     TOTAL = "Total"
     TITLE = "Counter"
+    FONT = ('Cascadia Code', '16')
 
-    def __init__(self, configs: List[Config]):
-        path_configs = [(Path(config.filename), config.display)
-                        for config in configs]
-        folders = set()
-        for file, _ in path_configs:
-            folder = file.parent
-            folders.add(folder)
+    @dataclass
+    class CountStringVar:
+        count: int
+        stringvar: tk.StringVar
 
-        self._observers: List[Observer] = []
-        for folder in folders:
+    _configs: Configs
+    _observers: List[Observer]
+    _event_queue: Queue[FileSystemEvent]
+    _root: tk.Tk
+    _file_to_display: Dict[Path, CountStringVar]
+    _total: CountStringVar
+    _remain_time: tk.StringVar
+
+    def __init__(self, configs: Configs):
+        self._configs = configs
+        self._observers = []
+        self._event_queue = Queue()
+        self._root = tk.Tk()
+        self._file_to_display = {}
+
+        self._init_root()
+        self._init_view()
+        self._init_observer()
+        self._init_loop()
+
+    def _init_root(self):
+        self._root.bind("<Destroy>", self._stop)
+        self._root.bind('<Control-c>', self._quit)
+        self._root.attributes('-topmost', True)
+
+    def _init_view(self):
+        self._root.title(App.TITLE)
+        for file in self._configs.files:
+            text = tk.StringVar()
+            count = texcount(file.path)
+            self._file_to_display[file.path] = App.CountStringVar(count, text)
+            create_label(self._root, file.display, count, text, App.FONT)
+        self._total = App.CountStringVar(self._get_total(), tk.StringVar())
+        create_label(self._root, App.TOTAL,
+                     self._total.count, self._total.stringvar, App.FONT)
+        self._remain_time = tk.StringVar()
+        tk.Label(self._root, textvariable=self._remain_time,
+                 font=App.FONT).pack(fill=tk.X, anchor="center")
+
+    def _init_observer(self):
+        for folder in set([f.path.parent for f in self._configs.files]):
             handler = CustomHandler(self)
             observer = Observer()
             observer.schedule(handler, folder, recursive=True)
             self._observers.append(observer)
-
-        self._queue = Queue[FileSystemEvent]()
-        self._root = tk.Tk()
-        self._root.title(App.TITLE)
-
-        self._file_to_display: Dict[Path, Tuple[int, tk.StringVar]] = {}
-        for file, display in path_configs:
-            text = tk.StringVar()
-            count = texcount(file)
-            self._file_to_display[file] = (count, text)
-            create_label(self._root, display, count, text)
-        self._total = (self.get_total(), tk.StringVar())
-        create_label(self._root, App.TOTAL, self._total[0], self._total[1])
-
-        self._root.bind("<Destroy>", self._stop)
-        self._root.bind(App.WATCHDOG_EVENT, self.handle_watchdog_event)
-        self._root.bind('<Control-c>', self._quit)
-        self._root.attributes('-topmost', True)
+        self._root.bind(App.WATCHDOG_EVENT, self._handle_watchdog_event)
         for observer in self._observers:
             observer.start()
 
-    def handle_watchdog_event(self, event):
+    def _init_loop(self):
+        def refresh_remain():
+            remaining_time = remain_time(self._configs.ddl)
+            self._remain_time.set(remaining_time)
+            self._root.after(1000, refresh_remain)
+        refresh_remain()
+
+    def _update_total(self):
+        new_total = self._get_total()
+        self._total.count = new_total
+        self._total.stringvar.set(str(new_total))
+
+    def _get_total(self) -> int:
+        return sum([x.count for x in self._file_to_display.values()])
+
+    def _handle_watchdog_event(self, event):
         """Called when watchdog posts an event"""
-        watchdog_event = self._queue.get()
+        watchdog_event = self._event_queue.get()
         if watchdog_event.event_type == EVENT_TYPE_MODIFIED:
             event_path = Path(watchdog_event.src_path)
             if event_path in self._file_to_display.keys():
                 count = texcount(event_path)
                 display_param = self._file_to_display[event_path]
-                display_param[1].set(str(count))
-                self._file_to_display[event_path] = (count, display_param[1])
-                self.update_total()
-
-    def update_total(self):
-        stringvar = self._total[1]
-        new_total = self.get_total()
-        stringvar.set(str(new_total))
-        self._total = (new_total, stringvar)
-
-    def get_total(self) -> int:
-        return sum([x[0] for x in self._file_to_display.values()])
+                display_param.count = count
+                display_param.stringvar.set(str(count))
+                self._update_total()
 
     def notify(self, event: FileSystemEvent):
         """Forward events from watchdog to GUI"""
-        self._queue.put(event)
+        self._event_queue.put(event)
         self._root.event_generate(App.WATCHDOG_EVENT, when="tail")
 
     def start(self):
@@ -135,14 +201,33 @@ class CustomHandler(FileSystemEventHandler):
         self._app.notify(event)
 
 
-def read_config(filename: str) -> List[Config]:
+def read_config(filename: str) -> Configs:
     """Read config file"""
-    return [Config(x['filename'], x['display']) for x in json.load(open(filename, 'r'))]
+    return RawConfigs.from_json(open(filename, 'r').read()).to_configs()
 
 
-app = App(read_config(sys.argv[1]))
+def remain_time(ddl: datetime) -> str:
+    """Format timedelta to string"""
+    now = datetime.now()
+    td = ddl - now
+    mm, ss = divmod(td.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    res = "剩余"
+    if td.days > 0:
+        return res + f"{td.days}天{hh:02d}时{mm:02d}分{ss:02d}秒"
+    elif hh > 0:
+        return res + f"{hh:02d}时{mm:02d}分{ss:02d}秒"
+    elif mm > 0:
+        return res + f"{mm:02d}分{ss:02d}秒"
+    elif ss > 0:
+        return res + f"{ss:02d}秒"
+    else:
+        return "寄"
 
-try:
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Count words in LaTeX files")
+    parser.add_argument("config", help="config file")
+    args = parser.parse_args()
+    app = App(read_config(args.config))
     app.start()
-except KeyboardInterrupt:
-    app.quit()
